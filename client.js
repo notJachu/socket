59c04e4be781c0666118b0e2d0b710a4d262ecf3
notJachu <jasiustasiu50@interia.pl>
2022-10-29 21:40:40 +0200

    basic server

--- client.js
+++ client.js
@@ -1,7 +1,25 @@
 var socket = io('http://localhost:3000');
+const count = document.getElementById('count');
+const input = document.getElementById('info');
+const message = document.getElementById('message');
+const button = document.getElementById('send');
 
-socket.on('test', (data) =>{
-    alert(data);
+//socket.on('test', (data) =>{
+//    alert(data);
+//})
+
+//socket.emit('cTest', 'hello server');
+
+var placeholder = Math.random() % 15;
+
+socket.emit('newPlayer', placeholder);
+
+function communication(){
+    socket.emit('update', input.value);
+}
+
+socket.on('userCount', data => {
+    count.innerHTML = data;
 })
 
-socket.emit('cTest', 'hello server');
+button.addEventListener('click', communication);
--- server.js
+++ server.js
@@ -5,19 +5,37 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let users = {};
 
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('test', 'hello')
-  socket.on('cTest', (data) => {
+io.on('connection', connected);
+
+function connected(socket){
+  socket.on('newPlayer', (data) => {
+    console.log("a new player connected with id: " + socket.id);
+    users[socket.id] = data;
+    console.log("user's special number: " +users[socket.id]);
+    var num = Object.keys(users).length
+    console.log("amount of players: " +num);
+    io.emit('userCount', num);
+  })
+  //socket.emit('test', 'hello')
+  socket.on('update', (data) => {
     console.log(data);
   })
-});
+  socket.on('disconnect', function() {
+    delete users[socket.id];
+    console.log(`player of id: ${socket.id} has disconnected`);
+    num = Object.keys(users).length;
+    console.log("amount of players: " +num);
+    io.emit('userCount', num);
+  })
+}
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
